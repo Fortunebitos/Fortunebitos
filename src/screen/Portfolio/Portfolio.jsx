a80158365c39dee27932a9e9e8af2472fade5da4
Fortunebitos <fortunebitos613@gmail.com>
2023-01-03 16:55:31 +0800

    fourth commit

--- src/screen/Portfolio/Portfolio.jsx
+++ src/screen/Portfolio/Portfolio.jsx
@@ -20,7 +20,7 @@ const Portfolio = ({ id }) => {
               </h1>
 
               <p>Front-End Developer</p>
-              <button className="cv">Download Cv</button>
+              <a href="https://github.com/Fortunebitos/resume/raw/main/Bitos-FortuneJr.-D.-Front-end-Dev%20(4).pdf" className="cv">Download Cv</a>
             </div>
           </Tilt>
         </div>
