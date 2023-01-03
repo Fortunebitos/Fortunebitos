72355caaad56f1c6c7741c95350859fc22f102a8
Fortunebitos <fortunebitos613@gmail.com>
2023-01-03 17:52:52 +0800

    final commit

--- src/screen/Project/Projects.jsx
+++ src/screen/Project/Projects.jsx
@@ -5,11 +5,11 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Autoplay, Pagination } from "swiper";
-const Projects = ({ id }) => {
+const Projects= ({ id }) => {
   return (
     <>
-      <div className="pt-main" id={id}>
-       <Swiper
+      <div className="tt-main" id={id}>
+         <Swiper
           modules={[Autoplay, Pagination]}
           style={{ marginBottom: "1rem" }}
           className="swipers"
@@ -17,12 +17,36 @@ const Projects = ({ id }) => {
           pagination={{ clickable: true }}
         >
           <SwiperSlide>
-        <div className="title" data-aos="fade-down-right" data-aos-duration="1000">
-          <span>
-            <b>Recent</b> Projects
-          </span>
+        <div className="ty-title" data-aos="fade-down" data-aos-duration="1000">
+          <span>Recent Projects</span>
+            </div>
+            
+        <div className="pt-project">
+          <div className="pt-too">
+          <span>ICCT STUDENT PORTAL</span>
+                
+                <p>We build Student Portal App in our thesis, to improve our portal in shcool. <br />ICCT Online Student Portal is an online gateway where students can access the<br /> registration/application system for new students, view grades, and update contact information. </p>
+                <div className="row-btn">
+                <div className="pt-btn">Html</div>
+                <div className="pt-btn1">Css</div>
+                <div className="pt-btn2">JavaScript</div>
+                <div className="pt-btn3">React Js</div>
+                <div className="pt-btn4">Sass</div>
+     </div></div>
+        <div className="pt-pic" data-aos="zoom-in-right" data-aos-duration="1000">
+        <img src="https://res.cloudinary.com/dkg6aexz0/image/upload/v1670487274/Group_2_rokbhe.png" alt="" className="img1"/>
+          </div>
+            </div>
+       
+
+
+        
+     
+          </SwiperSlide>
+           <SwiperSlide>
+        <div className="ty-title" >
+          <span>Recent Projects</span>
         </div>
-   
         <div className="pt-project">
           <div className="pt-too" data-aos="zoom-in-right" data-aos-duration="1000">
           <span>Soto Grande Cebu Hotel</span>
@@ -37,36 +61,12 @@ const Projects = ({ id }) => {
 </p>
      </div>
         <div className="pt-pic" data-aos="zoom-in-right" data-aos-duration="1000">
-        <img src="https://res.cloudinary.com/dkg6aexz0/image/upload/v1669883046/projects_recent_r1ulcf.png" className="img"/>
+        <img src="https://res.cloudinary.com/dkg6aexz0/image/upload/v1669883046/projects_recent_r1ulcf.png" alt="" className="img"/>
           </div>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
-             <div className="title" data-aos="fade-down-right" data-aos-duration="1000">
-          <span>
-            <b>Recent</b> Projects
-          </span>
-        </div>
-   
-           <div className="pt-project">
-          <div className="pt-too">
-          <span>ICCT STUDENT PORTAL</span>
-                
-                <p>We build Student Portal App in our thesis, to improve our portal in shcool. <br />ICCT Online Student Portal is an online gateway where students can access the<br /> registration/application system for new students, view grades, and update contact information. </p>
-                <div className="row-btn">
-                <div className="pt-btn">Html</div>
-                <div className="pt-btn1">Css</div>
-                <div className="pt-btn2">JavaScript</div>
-                <div className="pt-btn3">React Js</div>
-                <div className="pt-btn4">Sass</div>
-     </div></div>
-        <div className="pt-pic" data-aos="zoom-in-right" data-aos-duration="1000">
-        <img src="https://res.cloudinary.com/dkg6aexz0/image/upload/v1670487274/Group_2_rokbhe.png" className="img1"/>
-          </div>
-            </div>
-          </SwiperSlide>
-           </Swiper>
-        </div>
+            </SwiperSlide>
+        </Swiper>
+      </div>
     </>
   );
 };
